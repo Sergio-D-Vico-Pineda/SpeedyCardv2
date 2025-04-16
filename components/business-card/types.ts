0a689f34ee6e99fe48c114d98dbdbcaeec1b7dbe
Sergio David Vico Pineda <sergiodavid.vico@ipinformaticaprofesional.com>
2025-04-16 11:36:34 +0200

    add my own type and update updateCardData, i still don't know how it is working but dont touch it

--- components/business-card/types.ts
+++ components/business-card/types.ts
@@ -12,9 +12,25 @@ export interface BusinessCardData {
   textAlignment: 'left' | 'right' | 'center';
 }
 
+export interface MyCardData {
+  fuente: string;
+  size: number;
+  color: string;
+  bgcolor: string;
+  align: 'left' | 'right' | 'center';
+  tnombre: string;
+  tpuesto: string;
+  temail: string;
+  tempresa: string;
+  tphone: string;
+  twebsite: string;
+  turlimg: string;
+}
+
 export interface CardContextType {
   cardData: BusinessCardData;
-  updateCardData: (data: BusinessCardData) => void;
+  // updateCardData: (data: BusinessCardData | ((prev: BusinessCardData) => BusinessCardData)) => void;
+  updateCardData: (data: Partial<BusinessCardData> | ((prev: BusinessCardData) => BusinessCardData)) => void;
   isFlipped: boolean;
   toggleFlip: () => void;
 }
