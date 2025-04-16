29eb88cc9bfb0526237377d9d727c36ec1dd7f6a
Sergio David Vico Pineda <sergiodavid.vico@ipinformaticaprofesional.com>
2025-04-16 18:07:41 +0200

    WIP edit a url card

--- app/(tabs)/(cards)/index.tsx
+++ app/(tabs)/(cards)/index.tsx
@@ -2,28 +2,21 @@ import { useState, useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { BusinessCardEditor } from '@/components/business-card/BusinessCardEditor';
 import { useSearchParams } from 'expo-router/build/hooks';
-
-/* export default function EditScreen() {
-  return (
-    <View style={styles.container}>
-      <BusinessCardEditor />
-    </View>
-  );
-} */
+import type { MyCardData } from '@/types';
 
 export default function EditScreen() {
   const params = useSearchParams();
   const userid = params.get('id');
-  const [cardData, setCardData] = useState(null);
+  const [cardData, setCardData] = useState<MyCardData>();
 
   useEffect(() => {
     const fetchCardData = async () => {
       if (!userid) return;
 
       try {
-        const response = await fetch(`https://your-firebase-url.com/cards/${userid}`);
-        const data = await response.json();
-        setCardData(data);
+        // const response = await fetch(`https://your-firebase-url.com/cards/${userid}`);
+        // const data = await response.json();
+        // setCardData(data);
       } catch (error) {
         console.error('Error fetching card data:', error);
       }
