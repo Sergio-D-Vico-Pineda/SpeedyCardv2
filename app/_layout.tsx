30fb38d3fc95e30462a5d14521e4c3579a4b650a
Scarpy19 <nootravez19@gmail.com>
2025-04-18 23:26:22 +0200

    refactor: wrap RootLayout in StrictMode for enhanced debugging

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,18 +2,21 @@ import { Stack } from "expo-router";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { CardProvider } from '@/contexts/CardContext';
 import { DeepLinkHandler } from '@/components/DeepLinkHandler';
+import { StrictMode } from "react";
 
 export default function RootLayout() {
   return (
-    <AuthProvider>
-      <CardProvider>
-        <DeepLinkHandler />
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-          <Stack.Screen name="+not-found" options={{ title: 'Oops!' }} />
-        </Stack>
-      </CardProvider>
-    </AuthProvider>
+    <StrictMode>
+      <AuthProvider>
+        <CardProvider>
+          <DeepLinkHandler />
+          <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+            <Stack.Screen name="+not-found" options={{ title: 'Oops!' }} />
+          </Stack>
+        </CardProvider>
+      </AuthProvider>
+    </StrictMode>
   );
 }
