5d289034f3713c78986a367e90c2d7c7bd8a1b13
Scarpy19 <nootravez19@gmail.com>
2025-04-21 16:13:58 +0200

    disabled save if name empty

--- app/(tabs)/(cards)/_layout.tsx
+++ app/(tabs)/(cards)/_layout.tsx
@@ -1,12 +1,34 @@
-import { Tabs } from 'expo-router';
+import { router, Tabs } from 'expo-router';
 import { Edit, Layout, Save } from 'lucide-react-native';
+import { useCardContext } from '@/contexts/CardContext';
+import { TouchableOpacity } from 'react-native';
 
 export default function CardsLayout() {
   return (
     <Tabs screenOptions={{ headerShown: false, animation: 'fade', tabBarHideOnKeyboard: true }}>
       <Tabs.Screen name="index" options={{ title: 'Edit', tabBarIcon: ({ color, size }) => <Edit color={color} size={size} /> }} />
       <Tabs.Screen name="templates" options={{ title: 'Templates', tabBarIcon: ({ color, size }) => <Layout color={color} size={size} /> }} />
-      <Tabs.Screen name="save" options={{ title: 'Save', tabBarIcon: ({ color, size }) => <Save color={color} size={size} /> }} />
+      <Tabs.Screen
+        name="save"
+        options={{
+          title: 'Save',
+          tabBarIcon: ({ color, size }) => <Save color={color} size={size} />,
+          tabBarButton: (props) => {
+            const { cardData } = useCardContext();
+            const canSave = Boolean(cardData.tname && cardData.tname.trim().length > 0);
+
+            function gotosave() {
+              router.push("/(tabs)/(cards)/save")
+            }
+
+            return (
+              <TouchableOpacity disabled={!canSave} style={[props.style, { opacity: canSave ? 1 : 0.5 }]} onPress={gotosave}>
+                {props.children}
+              </TouchableOpacity>
+            );
+          },
+        }}
+      />
     </Tabs>
   );
 }
