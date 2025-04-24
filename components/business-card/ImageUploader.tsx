145d5aa120b1546e63a557e7206c2e145001ca35
Sergio David Vico Pineda <sergiodavid.vico@ipinformaticaprofesional.com>
2025-04-24 17:25:14 +0200

    adjust image picker

--- components/business-card/ImageUploader.tsx
+++ components/business-card/ImageUploader.tsx
@@ -28,7 +28,7 @@ export default function ImageUploader({ onImageSelect, onProfileImageSelect }: I
       }
 
       const result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        mediaTypes: ['images'],
         allowsEditing: true,
         aspect: [1, 1],
         quality: 0.8,
