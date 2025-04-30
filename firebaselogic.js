6b7d6e454b241b60361bcc1bbccce1d062cc9876
Sergio David Vico Pineda <sergiodavid.vico@ipinformaticaprofesional.com>
2025-04-30 18:25:28 +0200

    fix web auth

--- firebaselogic.js
+++ firebaselogic.js
@@ -1,23 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { getFirestore } from 'firebase/firestore';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
+import { getAuth } from "firebase/auth";
 import firebaseConfig from "./firebase";
 
-// Initialize Firebase
-// const analytics = getAnalytics(app);
-// const auth = getAuth(app);
 const app = initializeApp(firebaseConfig);
-const auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(AsyncStorage),
-}
-);
 const db = getFirestore(app);
+
+let auth;
+
+if (typeof window !== "undefined") {
+    // Web: use default getAuth
+    auth = getAuth(app);
+} else {
+    // Native: use initializeAuth with getReactNativePersistence
+    const { getReactNativePersistence, initializeAuth } = require("firebase/auth");
+    const AsyncStorage = require('@react-native-async-storage/async-storage').default;
+    auth = initializeAuth(app, {
+        persistence: getReactNativePersistence(AsyncStorage),
+    });
+}
 
 export {
     app,
