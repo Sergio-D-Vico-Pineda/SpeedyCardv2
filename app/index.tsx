b33ecb911669acdbca76d77c5e88d4ceeb8d7b00
Sergio David Vico Pineda <sergiodavid.vico@ipinformaticaprofesional.com>
2025-05-05 12:22:25 +0200

    refactor(index.tsx): simplify layout and improve button spacing

--- app/index.tsx
+++ app/index.tsx
@@ -10,50 +10,50 @@ export default function Index() {
 
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Image
-          source={require('@/assets/images/logo-icon.png')}
-          style={styles.logo}
-          resizeMode="contain"
-        />
-      </View>
+      <Image
+        source={require('@/assets/images/logo-icon.png')}
+        style={styles.logo}
+        resizeMode="contain"
+      />
       <View style={styles.content}>
         <Text style={styles.title}>Welcome to SpeedyCard</Text>
         <Text style={styles.subtitle}>Your Digital Card Solution</Text>
-
-        <Text style={styles.description}>
-          Create, manage, and share your digital business cards with ease.
-        </Text>
+        <Text style={styles.description}>Create, manage, and share your digital business cards with ease.</Text>
       </View>
-      {!user ? (
-        <View style={styles.minicontainer}>
-          <Link style={styles.button} href="/(auth)/login">
-            Login
-          </Link>
-          <Link style={styles.button} href="/(auth)/register">
-            Register
-          </Link>
-        </View>
-      ) : (
-        <View style={styles.minicontainer}>
-          <Link style={styles.button} href="/(tabs)">
-            Enter into the app
-          </Link>
-          <Pressable
-            style={styles.button}
-            onPress={() => setModalVisible(true)}
-          >
-            <Text style={styles.white}>Scan QR</Text>
-          </Pressable>
-          <Pressable onPress={signOut} style={styles.button}>
-            <Text style={styles.white}>Signout</Text>
-          </Pressable>
-          <ScanQRModal
-            visible={modalVisible}
-            onClose={() => setModalVisible(false)}
-          />
-        </View>
-      )}
+      <View style={styles.minicontainer}>
+        {!user ? (
+          <>
+            <Link style={styles.button} href="/(auth)/login">
+              Login
+            </Link>
+            <Link style={styles.button} href="/(auth)/register">
+              Register
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link style={styles.button} href="/(tabs)">
+              Enter into the app
+            </Link>
+            <Pressable onPress={signOut} style={styles.button}>
+              <Text style={styles.white}>Signout</Text>
+            </Pressable>
+          </>
+        )}
+        <Pressable
+          style={styles.button}
+          onPress={() => setModalVisible(true)}
+        >
+          <Text style={styles.white}>Scan QR</Text>
+        </Pressable>
+        <Link style={styles.button} href="/view?userid=sb1OHT4sN1aacyLzvCk7nXZhBLb2&card=1">
+          View a card
+        </Link>
+        <ScanQRModal
+          visible={modalVisible}
+          onClose={() => setModalVisible(false)}
+        />
+      </View>
     </View >
   );
 }
@@ -73,11 +73,10 @@ const styles = StyleSheet.create({
   header: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 20,
     color: '#fff',
   },
   content: {
-    flex: 0.6,
+    flex: 0.55,
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -104,17 +103,14 @@ const styles = StyleSheet.create({
     padding: 10,
     backgroundColor: '#1f6feb',
     borderRadius: 5,
-    marginBottom: 10,
     color: '#fff',
-    marginTop: 20
   },
   minicontainer: {
-    display: 'flex',
     flexDirection: 'row',
     justifyContent: 'center',
     flexWrap: 'wrap',
-    gap: 10,
-    marginBottom: 10,
+    columnGap: 10,
+    rowGap: 12,
     width: '80%',
   },
   white: {
