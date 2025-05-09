380b44842f3da4ed49efcb95b20ec0dd414e6288
Sergio David Vico Pineda <sergiodavid.vico@ipinformaticaprofesional.com>
2025-05-09 12:39:20 +0200

    feat(layout): integrate ToastProvider and ToastNotification

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,10 @@
 import { Stack } from "expo-router";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { CardProvider } from '@/contexts/CardContext';
+import { ToastProvider } from '@/contexts/ToastContext';
 import { StrictMode } from "react";
+import ToastNotification from '@/components/ToastNotification';
+
 export const unstable_settings = {
   // Ensure any route can link back to `/`
   initialRouteName: 'index',
@@ -12,12 +15,15 @@ export default function RootLayout() {
     <StrictMode>
       <AuthProvider>
         <CardProvider>
-          <Stack screenOptions={{ headerShown: false, headerStyle: { backgroundColor: '#25292e' } }}>
-            <Stack.Screen name="index" options={{ headerShown: false }} />
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-            <Stack.Screen name="+not-found" options={{ title: 'Oops!' }} />
-          </Stack>
+          <ToastProvider>
+            <Stack screenOptions={{ headerShown: false, headerStyle: { backgroundColor: '#25292e' } }}>
+              <Stack.Screen name="index" options={{ headerShown: false }} />
+              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+              <Stack.Screen name="+not-found" options={{ title: 'Oops!' }} />
+            </Stack>
+            <ToastNotification />
+          </ToastProvider>
         </CardProvider>
       </AuthProvider>
     </StrictMode>
